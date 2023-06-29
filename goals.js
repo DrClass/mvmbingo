b385b0502b51704409a7cea306d4ab1556206996
BuildTools <unconfigured@null.spigotmc.org>
2023-06-29 17:40:58 -0400

    initial commit

---
+++ goals.js
@@ -0,0 +1,59 @@
+// Short Text|id|Description (optional)
+
+easyGoals = [
+"0 tour sighting|0tour",
+"Gas Passer Pyro|gaspasser",
+"Wave 1 Leaver|earlyleaver",
+"F4 bind|f4bind",
+"Double Medic|doublemed",
+"Double Engineer|doubleengi",
+"Knockback Rage Heavy|knockbackrage",
+"Racist Objector sign|objectorracist",
+"NSFW Objector sign|objectornsfw",
+"\"Good Luck on Drops\" chat message|gldrops",
+"Racist chat message|racistchat",
+"Wave Lost|wavelost",
+"100+ tour sighting|100tour",
+"Pyro dies in first 10s of wave|pyrofastdeath",
+"Someone refuses to read chat|nochat",
+"Someone has voice chat off|novoice",
+"Pre-round canteen use|accidentalcanteen",
+]
+
+mediumGoals = [
+"MARI sighting|mari|Must have MARI in their username or be in the MvM Animal Research Institute Steam group",
+"500+ tour sighting|500tour",
+"Stock Medigun|stockmedi|Stock Medigun or any of its varients/reskins",
+"Demoknight|demoknight",
+"AFK mid wave|afk",
+"F4 Objector sign|objectorf4",
+"Player gets kicked|kicked",
+"All knowing low tour|lowtourboss|<10 tour giving out orders",
+"Soldier kills teammate with reflected projectile|soldierreflectkill",
+"No dispenser range|nodisprange",
+"Hackuzation|hackuzation|Someone gets accused of cheating",
+"Heavy shoots tank instead of giants|heavytank",
+"Medic revives teammate into buster|busterres",
+"Someone demands a Medic|demandmedic",
+]
+
+hardGoals = [
+"Diaper Telleporter|diaper|Must be an intentional diaper",
+"Rocket/Sticky Jumper|jumper",
+"Disposible Sentry|disposiblesentry",
+"Cheater/Hacker|cheater",
+"Airblast Griefing|griefingairblast|Pushing bots forwards or busters into teammates",
+"Auto F4 script,f4script",
+"Super Scout bomb cap|superscoutcap",
+"Crit sticky spammer|critsticks",
+"Fall damage death|falldamage|Someone on the team dies to fall damage",
+"Full stock loadout|stock|Includes no reskins",
+"200+ ping|highping|Someone has 200+ ping",
+];
+
+
+var exclusions = [
+["doublemed", "doubleengi"],
+["objectorracist", "objectornsfw", "objectorf4"],
+["500tour", "100tour"],
+];
